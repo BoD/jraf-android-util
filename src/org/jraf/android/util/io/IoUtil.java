fa422b2c047c811215774632c7540effdf8384e0
BoD <BoD@JRAF.org>
2013-06-08 01:14:37 +0200

    - IoUtil.copy() - IoUtil.readFully()

--- src/org/jraf/android/util/io/IoUtil.java
+++ src/org/jraf/android/util/io/IoUtil.java
@@ -25,8 +25,12 @@ package org.jraf.android.util.io;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IoUtil {
+    private static final int BUFFER_SIZE = 1500;
+
     /**
      * Silently close the given {@link Closeable}s, ignoring any {@link IOException}.<br/> {@code null} objects are ignored.
      * 
@@ -43,4 +47,45 @@ public class IoUtil {
             }
         }
     }
+
+    /**
+     * Copy the contents of the given {@link InputStream} into the given {@link OutputStream}.<br/>
+     * Note: the given {@link InputStream} and {@link OutputStream} won't be closed.
+     * 
+     * @param in The {@link InputStream} to read.
+     * @param out The {@link OutputStream} to write to.
+     * @return the actual number of bytes that were read.
+     * @throws IOException If a error occurs while reading or writing.
+     */
+    public static long copy(InputStream in, OutputStream out) throws IOException {
+        long res = 0;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+            out.flush();
+            res += read;
+        }
+        return res;
+    }
+
+    /**
+     * Fully reads the given {@link InputStream} into a {@link String}.<br/>
+     * The encoding inside the {@link InputStream} is assumed to be {@code UTF-8}.<br/>
+     * Note: the given {@link InputStream} won't be closed.
+     * 
+     * @param in The {@link InputStream} to read.
+     * @return a String containing the contents of the given {@link InputStream}.
+     * @throws IOException If a error occurs while reading.
+     */
+    public static String readFully(InputStream in) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            stringBuilder.append(new String(buffer, 0, read, "utf-8"));
+        }
+        return stringBuilder.toString();
+    }
+
 }
