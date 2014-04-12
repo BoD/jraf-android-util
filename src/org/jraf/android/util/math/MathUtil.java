6b6bde3fdf12f2796692eebc6a1950ca96ae9b7a
BoD <BoD@JRAF.org>
2014-04-12 15:45:10 +0200

    User varargs.

--- src/org/jraf/android/util/math/MathUtil.java
+++ src/org/jraf/android/util/math/MathUtil.java
@@ -30,7 +30,7 @@ public class MathUtil {
      * @param values The array in which to look for the min and max values.
      * @return an array containing the min at index 0 and the max at index 1.
      */
-    public static float[] getMinMax(float[] values) {
+    public static float[] getMinMax(float... values) {
         if (values.length == 0) return new float[] { Float.MIN_VALUE, Float.MAX_VALUE };
         float min = values[0];
         float max = values[0];
@@ -48,12 +48,24 @@ public class MathUtil {
     }
 
     /**
+     * Get the amplitude (difference between the min and the max) of the given array.
+     * 
+     * @param values The array for which to calculate the amplitude.
+     * @return The amplitude of the given arrat.
+     */
+    public static float getAmplitude(float... values) {
+        if (values.length == 0) return 0;
+        float[] minMax = getMinMax(values);
+        return minMax[1] - minMax[0];
+    }
+
+    /**
      * Get the average of the given array.
      * 
      * @param values The array in which to calculate the average.
      * @return the average.
      */
-    public static float getAverage(float[] values) {
+    public static float getAverage(float... values) {
         float sum = 0;
         for (float val : values) {
             sum += val;
